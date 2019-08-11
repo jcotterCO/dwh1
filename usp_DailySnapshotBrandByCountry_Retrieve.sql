1c962fd11a4173074891e5312b9712e47cfc54f3
jcotterCO <54008535+jcotterCO@users.noreply.github.com>
2019-08-12 09:26:42 +1000

    Update usp_DailySnapshotBrandByCountry_Retrieve.sql

--- usp_DailySnapshotBrandByCountry_Retrieve.sql
+++ usp_DailySnapshotBrandByCountry_Retrieve.sql
@@ -1,6 +1,16 @@
+-- =============================================
+-- Author:		Avanade
+-- Create date: 01-01-2019
+-- Description:	Retrieve data from FACTSALES for use in Daily Snapshot Brand By Country
+-- 
+-- Edit History
+-- Date 			Author 				Changes
+-- 08/08/19 		Jaymes Cotter 		Adding new returnable fields to containing LY Local Sales @ Current AUD FX Rate AUD in order to correctly calculate Comps and GP. 
+-- =============================================
+
 USE [CDM]
 GO
-/****** Object:  StoredProcedure [rpt].[usp_DailySnapshotBrandByCountry_Retrieve]    Script Date: 12/08/2019 9:06:19 AM ******/
+/****** Object:  StoredProcedure [rpt].[usp_DailySnapshotBrandByCountry_Retrieve]    Script Date: 8/08/2019 3:20:44 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -37,6 +47,10 @@ AS
 		 SUM(CASE WHEN DoorCompFlag = 1 THEN [FullMonthSalesLastYearAUD] ELSE 0 END) 									AS 'Sales_LY_FullMth_Door',--
 		 SUM(CASE WHEN DoorCompFlag = 1 THEN [FullMonthBudgetSalesLastYearAUD] ELSE 0 END) 								AS 'Sales_LY_FullMthB_Door',--
 
+		 -- SUM(CASE WHEN DoorCompFlag = 1 THEN [DaySalesLastYearCompAUD] ELSE 0 END) 										AS 'Sales_LY_DailyA_Comp_Door',
+		 -- SUM(CASE WHEN DoorCompFlag = 1 THEN [MTDSalesLastYearCompAUD] ELSE 0 END) 										AS 'Sales_LY_MTDA_Comp_Door',
+		 -- SUM(CASE WHEN DoorCompFlag = 1 THEN [FullMonthSalesLastYearCompAUD] ELSE 0 END) 								AS 'Sales_LY_FullMth_Comp_Door',
+
 		 SUM([DaySalesThisYearAUD]) 																					AS 'Sales_DailyA',
 		 SUM([MTDSalesThisYearAUD])																						AS 'Sales_MTDA', 
 		 SUM([MTDBudgetSalesThisYearAUD])																				AS 'Sales_MTDB', 
@@ -49,15 +63,30 @@ AS
 		 SUM([FullMonthSalesLastYearAUD])																				AS 'Sales_LY_FullMth',
 		 SUM([FullMonthBudgetSalesLastYearAUD])																			AS 'Sales_LY_FullMthB',
 
+		 /* NEW */
+		 -- SUM([DaySalesLastYearCompAUD]) 																					AS 'Sales_LY_Comp_DailyA',
+		 -- SUM([MTDSalesLastYearCompAUD])																					AS 'Sales_LY_Comp_MTDA', 
+		 -- SUM([FullMonthSalesLastYearCompAUD])																			AS 'Sales_LY_Comp_FullMth',
+
 		 SUM([DayGrossProfitThisYearAUD]) 																				AS 'GPDollar_DailyA',
 		 SUM([MTDGrossProfitThisYearAUD])																				AS 'GPDollar_MTDA', 
 		 SUM([MTDBudgetGrossProfitThisYearAUD])																			AS 'GPDollar_MTDB', 
 		 SUM([FullMonthBudgetGrossProfitThisYearAUD])																	AS 'GPDollar_FullMthB',
 
+		 /* NEW */
+		 -- SUM([DayGrossProfitLastYearCompAUD]) 																			AS 'GPDollar_Comp_DailyA',
+		 -- SUM([MTDGrossProfitLastYearCompAUD])																			AS 'GPDollar_Comp_MTDA', 
+		 -- SUM([FullMonthGrossProfitLastYearCompAUD])																		AS 'GPDollar_Comp_FullMthB',
+
 		 SUM(CASE WHEN DoorCompFlag = 1 THEN [DayGrossProfitThisYearAUD] ELSE 0 END) 									AS 'GrossProfit_DailyA_Door',
 		 SUM(CASE WHEN DoorCompFlag = 1 THEN [DayGrossProfitLastYearAUD] ELSE 0 END) 									AS 'GrossProfit_LY_DailyA_Door',
 	 	 SUM(CASE WHEN DoorCompFlag = 1 THEN [MTDGrossProfitThisYearAUD] ELSE 0 END) 									AS 'GrossProfit_MTDA_Door',
-		 SUM(CASE WHEN DoorCompFlag = 1 THEN [MTDGrossProfitLastYearAUD] ELSE 0 END) 									AS 'GrossProfit_LY_MTDA_Door'
+		 SUM(CASE WHEN DoorCompFlag = 1 THEN [MTDGrossProfitLastYearAUD] ELSE 0 END) 									AS 'GrossProfit_LY_MTDA_Door'--,
+		
+		/* NEW */
+		-- SUM(CASE WHEN DoorCompFlag = 1 THEN [DayGrossProfitLastYearCompAUD] ELSE 0 END) 									AS 'GrossProfit_LY_DailyA_Comp_Door',
+		-- SUM(CASE WHEN DoorCompFlag = 1 THEN [MTDGrossProfitLastYearCompAUD] ELSE 0 END) 									AS 'GrossProfit_LY_MTDA_Comp_Door'
+
 		FROM [rpt].[ReportData] T
 		WHERE 
 					  (@Division	IS NULL OR		@Division =	T.Division)
